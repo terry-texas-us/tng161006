ca14b748ab00401e86ff48161b8c8e662cf1a0c9
Terry A. Smith <t.smith@swbell.net>
2016-03-20 07:15:27 -0500

    simple cache for textSnippets

--- _/components/php/textSnippets.php
+++ _/components/php/textSnippets.php
@@ -1,16 +1,16 @@
 <?php
 
 function uiTextSnippet($name, $extra = null) {
+  static $snippets = [];
   if (!isset($name) || $name === '') {
     $value = '';
+  } else if (array_key_exists($name, $snippets)) {
+    $value = $snippets[$name];
   } else {
     $language = $_SESSION['session_language'];
 
-    $query = "SELECT value "
-         . "FROM snippets "
-         . "JOIN languages ON snippets.languageID = languages.languageID "
-         . "WHERE snippets.name = '$name' "
-         . "AND languages.folder = '$language';";
+    $query = "SELECT value FROM snippets JOIN languages ON snippets.languageID = languages.languageID "
+         . "WHERE snippets.name = '$name' AND languages.folder = '$language';";
 
     $result = tng_query($query);
     $row = tng_fetch_assoc($result);
@@ -18,7 +18,7 @@ function uiTextSnippet($name, $extra = null) {
 
     $value = $row && array_key_exists('value', $row) ? $row['value'] : '';
 
-    if ($value == '') {
+    if ($value === '') {
       if (preg_match("/^[iIfF][0-9]+$/", $name)) { // [ts] Individual (i###) and Family (f###) ids are passed to this function. Modify callers to always call with values expected to be snippets?
         echo("<script>console.log('Undefined User Interface Text Snippet: [" . $name . "]');</script>");
       } else if (preg_match("/^\d{4}.+\d{4}$/", $name)) {  // [ts] rm date year date range ex. 1899-1901. Notice the .+ to interprete the unicode dash/hyphen codes.
@@ -26,6 +26,8 @@ function uiTextSnippet($name, $extra = null) {
       } else {
         trigger_error('Undefined User Interface Text Snippet: [' . $name . ']');
       }
+    } else {
+      $snippets[$name] = $value;
     }
   }  
   // [ts] this extra is likely not worth the effort .. looks clunky when used with <b>, <i>, <em>
