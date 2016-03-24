99845d467f3ed8cbf3ec42e8cbd23e553c463b63
Terry A. Smith <t.smith@swbell.net>
2016-03-24 11:37:30 -0500

    indentation

--- repositories.php
+++ repositories.php
@@ -1,20 +1,20 @@
 <?php
 
 function buildRepositoryMenu($currpage, $repositoryId) {
-  global $tree;
-  global $allowEdit;
-  global $rightbranch;
-  global $emailaddr;
-  
-  $menu = '';
-  if ($allowEdit && $rightbranch) {
-    $menu .= "<a id='a0' href='repositoriesEdit.php?repoID=$repositoryId&amp;tree=$tree&amp;cw=1' title='" . uiTextSnippet('edit') . "'>\n";
-      $menu .= "<img class='icon-sm' src='svg/new-message.svg'>\n";
-    $menu .= "</a>\n";
-  } elseif ($emailaddr && $currpage != 'suggest') {
-    $menu .= "<a id='a0' href='repositorySuggest.php?ID=$repositoryId&amp;tree=$tree' title='" . uiTextSnippet('suggest') . "'>\n";
-      $menu .= "<img class='icon-sm' src='svg/new-message.svg'>\n";
-    $menu .= "</a>\n";
-  }
-  return $menu;
+    global $tree;
+    global $allowEdit;
+    global $rightbranch;
+    global $emailaddr;
+
+    $menu = '';
+    if ($allowEdit && $rightbranch) {
+        $menu .= "<a id='a0' href='repositoriesEdit.php?repoID=$repositoryId&amp;tree=$tree&amp;cw=1' title='" . uiTextSnippet('edit') . "'>\n";
+          $menu .= "<img class='icon-sm' src='svg/new-message.svg'>\n";
+        $menu .= "</a>\n";
+    } elseif ($emailaddr && $currpage != 'suggest') {
+        $menu .= "<a id='a0' href='repositorySuggest.php?ID=$repositoryId&amp;tree=$tree' title='" . uiTextSnippet('suggest') . "'>\n";
+          $menu .= "<img class='icon-sm' src='svg/new-message.svg'>\n";
+        $menu .= "</a>\n";
+    }
+    return $menu;
 }
